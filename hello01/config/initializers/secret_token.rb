af43d02aaada70d5922600649bf408a59be10412
Craig Smitham <craigsmitham@gmail.com>
2012-09-30 16:53:08 -0500

    initial commit

---
+++ hello01/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello01::Application.config.secret_token = '29f7caf058133814ebf54f597722e4e5100a73c41046295d8d81637b430ed6cc37e4c010f9444767b3ab128d296e0f36adbf11ab74b5d07b721b527a1e540ea0'
